56a30400ac2a83285a0e1b8f4071e5bb10d0789c
Eric Nilsson <CaterHatterPillar@gmail.com>
2016-10-19 19:00:11 +0200

    Add Kernighan method

--- main.c
+++ main.c
@@ -2,13 +2,15 @@
 #include <stdio.h>
 
 #include "builtin_count_bits.h"
+#include "kernighan_count_bits.h"
 #include "lookup_count_bits.h"
 #include "naive_count_bits.h"
 
 int main(int argc, char* argv[]) {
   test_builtin_count_bits();
-  test_naive_count_bits();
+  test_kernighan_count_bits();
   test_lookup_count_bits();
+  test_naive_count_bits();
   
   return 0;
 }
